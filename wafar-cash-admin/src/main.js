bad47a7c5fe7f183cbf6fe456a5a5791f85b82ee
faresghaben <faresgaben@gmail.com>
2026-08-24 20:46:35 +0300

    Clean commit with proper gitignore

--- wafar-cash-admin/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router.js'
-import './assets/styles.css'
-
-// إنشاء تطبيق Vue الرئيسي وربط الراوتر
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
